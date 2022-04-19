2f2edfc801d92a10b941c061eb24898c7a2ae1bb
Park <e2196102@cmaisonneuve.qc.ca>
2022-04-19 13:05:32 -0400

    adding map and timetable section and changing the border radius

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,16 +3,16 @@
 let hamburger = document.querySelector(".navigation__hamburger");
 let navContent = document.querySelector(".navigation__content");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", function() {
     hamburger.classList.toggle("active");
     navContent.classList.toggle("active");
-})
+});
 
 // navigation hiding (scroll down) and showing (scroll up) 
 
 let lastScrollTop = window.scrollY,
     navbar = document.querySelector('[data-navigation]');
-    window.addEventListener('scroll', () => {
+    window.addEventListener('scroll', function() {
         if (navContent.classList.contains("active") && hamburger.classList.contains("active")){
             return
         }
